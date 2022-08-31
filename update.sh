3422e3cfa8d34d3ddbf8f0582205c4f0db43dc54
Malthe <malthegram22@gmail.com>
2022-08-31 11:33:33 +0200

    teaching stuff

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ declare -a repos=(
 	#"brands-whitelabel"
 	"costume"
 	"bobedre"
-	# "boligmagasinet"
+	"boligmagasinet"
 	# "illustreret"
 	# "historienet"
 	# "iform"
@@ -16,37 +16,19 @@ declare -a repos=(
 
 for i in "${repos[@]}"
 do
-	if [[ -d ! ${i} ]]; then
-		cd ../GitHub
-		git clone git@github.com:BenjaminMedia/${i}.git
-		echo "${GREEN}Done cloning ${i}..."
-		cd ${i}
-		echo "Adding brands-whitelabel as template...${NC}"
-		git remote add template ${WL}	
-		echo
-		echo "${GREEN}Template Added!${NC}"
-		echo
-		echo "${GREEN}Installing Yarn..⏳${NC}"
-		echo
-		yarn install
-		echo "${GREEN}Installing Yarn complete!${NC}"
-	else
-		echo "${i} already exists!"
-	fi
+	cd ../GitHub
+	git clone git@github.com:BenjaminMedia/${i}.git
+	echo "${GREEN}Done cloning ${i}..."
+	cd ${i}
+	echo "Adding brands-whitelabel as template...${NC}"
+	git remote add template ${WL}	
+	echo
+	echo "${GREEN}Template Added!${NC}"
+	echo
+	echo "${GREEN}Installing Yarn..⏳${NC}"
+	echo
+	yarn install
+	echo "${GREEN}Installing Yarn complete!${NC}"
+
 
 done
-
-
-# cd C:/Users/malthe/Documents/GitHub
-# git clone git@github.com:BenjaminMedia/costume.git $COS
-# echo -e "${GREEN}Done Cloning $COS$" 
-# echo
-# echo -e "${GREEN}Adding WL template..."
-# sleep 1s
-# cd $COS 
-# git remote add template git@github.com:BenjaminMedia/brands-whitelabel.git
-# echo -e "${GREEN}Installing yarn...${NC}"
-# yarn install
-# sleep 1s
-# echo -e "${GREEN}Done with $COS"
-
